b71f874179f45f0a79ae4bcecb58a333a8366b93
amantaya <aantaya@email.arizona.edu>
2026-08-13 13:45:45 -0600

    Update table names in constraints query for consistency

--- SQL/constraints.sql
+++ SQL/constraints.sql
@@ -6,7 +6,7 @@ FROM
     information_schema.table_constraints AS tc
 WHERE
     tc.table_schema = 'main'
-    AND tc.table_name IN ('animals', 'gps_fixes', 'weights')
+    AND tc.table_name IN ('animals_tbl', 'gps_tbl', 'weights_tbl')
 ORDER BY
     tc.table_name,
     tc.constraint_type,
